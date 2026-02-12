d1aad2d44775e33a0a4da38d6d347e0c95dda68a
Cihilist <calinstefanfodor@email.com>
2026-02-12 12:25:11 +0200

    Adapted client main function completly

--- js/main.js
+++ js/main.js
@@ -6,9 +6,6 @@ async function startApp()
     const phoneManager = new PhoneManager(`ws://${window.location.host}`, (data) => ui.renderBars(data));
     const ui = new UI(phoneManager);
 
-    await ui.renderBars();    
-
-    setInterval(() => ui.renderBars(), 2000);
 }
 
 startApp();
